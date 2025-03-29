ce170f69b7d5fb06036c32acbe51e705b975670d
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-03-29 13:25:27 +0530

    commit llama3.py  code

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ## 1. Create a conda environment
 
 ```bash
-conda create -n venvAwsBEDROCK python==3.13
+conda create -n venvAws python==3.10 -y
 ```
 
 ## 2. Install the required packages
@@ -16,4 +16,15 @@ conda create -n venvAwsBEDROCK python==3.13
 python -m pip install -r requirements.txt
 ```
 
-## 3.
+## 3. Configure AWS credentials
+
+```bash
+aws configure
+```
+
+## 4. run that llama3.py file
+````bash
+python llama3.py
+````
+
+## 5.
---
+++ llama3.py
@@ -0,0 +1,43 @@
+import boto3
+import json
+
+
+prompt_data="""
+Act as a shakespeare and write a poem on Machine Learning
+
+"""
+
+AWS_REGION = "us-east-1"
+
+# this client we will use and the service name is bedrock-runtime
+bedrock=boto3.client(service_name="bedrock-runtime",region_name=AWS_REGION)
+
+
+# structure of the payload and this will be key value pair
+payload={
+    "prompy":"[INST]" + prompt_data + "[/INST]",
+    "max_gen_len":512,
+    "temperature":0.5,
+    "top_p":0.9
+}
+
+
+## convert into json
+body=json.dumps(payload)
+
+
+model_id="meta.llama3-3-70b-instruct-v1:0"
+
+
+response=bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+
+response_body=json.loads(response.get("body").read())
+response_text=response_body['generation']
+print(response_text)
+
+
